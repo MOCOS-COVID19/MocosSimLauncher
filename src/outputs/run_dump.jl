8897afedf0396de368703498353bb8eb922504bf
Marcin Bodych <bodychmarcin@gmail.com>
2023-05-16 02:08:11 +0200

    detections added

--- src/outputs/run_dump.jl
+++ src/outputs/run_dump.jl
@@ -12,6 +12,7 @@ function pushtrajectory!(d::RunDump, trajectory_id::Integer, writelock::Base.Abs
       dict = JLD2.Group(f, string(trajectory_id))
       num_individuals = MocosSim.numindividuals(state)
       infection_times = Vector{OptTimePoint}(missing, num_individuals)
+      detection_times = Vector{OptTimePoint}(missing, num_individuals)
       death_times = Vector{OptTimePoint}(missing, num_individuals)
       hosp_times = Vector{OptTimePoint}(missing, num_individuals)
       hospitalization_progressions = getproperty.(state.progressions, :severe_symptoms_time)
@@ -20,9 +21,11 @@ function pushtrajectory!(d::RunDump, trajectory_id::Integer, writelock::Base.Abs
         event = MocosSim.backwardinfection(state, i)
         kind = contactkind(event)
         infection_times[i] = ifelse(kind == MocosSim.NoContact, missing, time(event))
+        detection_times[i] = callback.detection_times[i]
         death_times[i] = infection_times[i] + death_progressions[i]
         hosp_times[i] = infection_times[i] + hospitalization_progressions[i]
       end
+      dict["detections"] = detection_times
       dict["infections"] = infection_times
       dict["deaths"] = death_times
       dict["hospitalizations"] = hosp_times
